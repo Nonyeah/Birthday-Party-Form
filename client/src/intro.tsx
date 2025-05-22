9bc958462d0af0e69a2af2c018615d2bbd2fba34
nonyeah <nonyeulasi@hotmail.com>
2025-05-22 16:43:44 +0100

    initial commit made to repo

---
+++ client/src/intro.tsx
@@ -0,0 +1,24 @@
+
+export default function Intro() {
+  return (
+    <div className="intro-container">
+      <h4>November 1st 2025</h4>
+
+      <h1>Our beloved mother turns 80 this year!</h1>
+
+      <p>To Christ Jesus be the glory!</p>
+      <p>
+        Come help us celebrate the 80th birthday of our beloved mother,
+        grandmother, sister and friend to many, Mrs Ethel Ulasi, who on 31st October
+        2025 will have been alive to witness the earth rotate around the sun 80
+        times, wow! We are truly grateful to God for blessing our dear mother with
+        such a long and fruitful life and we pray our Good God will continue to
+        satisfy her with his everlasting goodness.
+      </p>
+      <p>
+        Come join us as we celebrate! Let's eat some delicious food, drink until
+        we are merry and <p><span>DANCE, DANCE, DANCE!</span></p>
+      </p>
+    </div>
+  );
+}
---
+++ server/api.js
@@ -0,0 +1,66 @@
+import express from "express";
+import path from "path";
+import cors from "cors";
+import fs from "fs";
+import bodyParser from "body-parser";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(
+  cors({
+    origin: "https://ethels-80th-birthday.online",
+    methods: ["GET", "POST"],
+  })
+);
+
+app.use(bodyParser.json());
+app.use(express.static(path.resolve(__dirname, "dist")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "dist", "index.html"));
+});
+
+app.post("/api", (req, res) => {
+  console.log("Request body:", req.body);
+  const { name, email, attending, otherguests } = req.body;
+  const [firstName, lastName] = name.split(" ");
+
+  const tempArray = [];
+
+  try {
+    const fileData = fs.readFileSync("invitationList.txt", "utf-8");
+    if (fileData) {
+      const guestList = JSON.parse(fileData);
+      tempArray.push(...guestList);
+    }
+  } catch (err) {
+    console.log("No existing file or invalid JSON.");
+  }
+
+  tempArray.push(req.body);
+
+  fs.writeFile(
+    "invitationList.txt",
+    JSON.stringify(tempArray, null, 4),
+    (err) => {
+      if (err) console.log(err);
+    }
+  );
+
+  if (attending) {
+    res.send(
+      `That's awesome ${firstName}, we look forward to seeing you on 1st November!`
+    );
+  } else {
+    res.send(`Awww that's a pity ${firstName}, we're sorry you can't make it.`);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at https://ethels-80th-birthday.online:${PORT}`);
+});
